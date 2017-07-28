8615e5eb574b40f611f793890d055be11026e34a
Sokunbi Taiwo <sokunbitaiwo82@gmail.com>
2017-07-28 06:32:40 +0100

    feature(create-models): - create user model - create document model - create role model
    [Finishes #149463343]

---
+++ server/models/document.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+  const Document = sequelize.define('Document', {
+    title: DataTypes.STRING,
+    content: DataTypes.TEXT,
+    access: DataTypes.STRING,
+    roleId: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER
+  }, {
+    classMethods: {
+      associate: (models) => {
+        Document.belongsTo(models.Role, {
+          foreignKey: 'roleId',
+          onDelete: 'CASCADE',
+        });
+        Document.belongsTo(models.User, {
+          foreignKey: 'userId',
+          onDelete: 'CASCADE',
+        });
+      }
+    }
+  });
+  return Document;
+};
