387ba1bfbb862a9b5e0105a47785899840df2838
Sokunbi Taiwo <sokunbitaiwo82@gmail.com>
2017-08-10 19:54:19 +0100

    fix(database error)

--- server/config.js
+++ server/config.js
@@ -12,17 +12,17 @@ module.exports = {
     jwtSecret: null,
     jwtSession: { session: false }
   },
-  test: {
-    username: 'taiwo',
-    password: null,
-    database: 'database_test',
-    host: 'localhost',
-    dialect: 'postgres'
-  },
   // test: {
-  //   use_env_variable: 'DATABASE_URL',
+  //   username: 'taiwo',
+  //   password: null,
+  //   database: 'database_test',
+  //   host: 'localhost',
   //   dialect: 'postgres'
   // },
+  test: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres'
+  },
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres'
