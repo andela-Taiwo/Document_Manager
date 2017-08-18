860eb3e3368ce4852819b5f5b40a3898ac27fa8a
Sokunbi Taiwo <sokunbitaiwo82@gmail.com>
2017-08-18 10:36:50 +0100

    fix(bug):  -fix bug in app.js

--- server/app.js
+++ server/app.js
@@ -31,12 +31,12 @@ app.use(expressValidator({
     };
   }
 }));
-app.use(express.static('client'));
+app.use(express.static('documentation'));
 
 // Require our routes into the application.
 require('./route')(app);
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
