d09623fdc9795ec2ee2729118899057e19e68248
Sokunbi Taiwo <sokunbitaiwo82@gmail.com>
2017-09-06 13:37:34 +0100

    chore(client setup): - add webpack config - add client directory - setup client server - add basic components

---
+++ client/src/components/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+class HomePage extends React.Component {
+  constructor(props) {
+    super();
+  }
+
+  render(){
+    return(
+      <div> <h1> Welcome to Reliable DOCS</h1> </div>
+
+    );
+  }
+}
+
+export default HomePage;
